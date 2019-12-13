2ddb84853f11f1ef9b42292bbac6c152153377da
snazau <snazarikov@gmail.com>
2019-12-13 22:08:40 +0200

    trash#2

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from config import *
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger()
 
-bot = telebot.TeleBot(Config.BOT_TOKEN)
+bot = telebot.TeleBot(Config.get_token())
 
 @bot.message_handler(
     content_types=['text', 'audio', 'document', 'photo', 'sticker', 'video', 'video_note', 'voice', 'location',
@@ -13,12 +13,16 @@ bot = telebot.TeleBot(Config.BOT_TOKEN)
                    'delete_chat_photo', 'group_chat_created', 'supergroup_chat_created', 'channel_chat_created',
                    'migrate_to_chat_id', 'migrate_from_chat_id',
                    'pinned_message'])
-def sending_auto2(message):
+def answer_smth(message):
     bot.send_message(chat_id=message.chat.id, text=autosending_text(bot, message), disable_web_page_preview=True)
 
-if Config.MODE == "PROD":
-	logger.debug("Config.MODE =", Config.MODE)
+MODE = Config.get_mode()
+TOKEN = Config.get_token()
 
 if __name__ == '__main__':
-	logging.debug("Config.MODE", Config.MODE)
-	bot.polling()  # Заставляет бота получать уведомления о новых сообщениях
+	logging.info("Selected mode " + mode)
+	if MODE == "debug":
+		bot.polling()
+		bot.remove_webhook()
+	else:
+		bot.set_webhook(Config.get_url() + TOKEN)
--- config.py
+++ config.py
@@ -2,14 +2,28 @@ import os
 
 from telebot.types import InlineKeyboardButton
 
+class Config:    
+    @staticmethod
+    def get_mode():
+        MODE = os.environ.get('MODE')
+        if MODE != "prod":
+        	MODE = "debug"
+        return MODE
 
-class Config:
-    BOT_TOKEN = os.environ['BOT_TOKEN']
-    MODE = os.environ['MODE']
+    @staticmethod
+    def get_token():
+    	BOT_TOKEN = os.environ.get('BOT_TOKEN')
+    	if BOT_TOKEN is None:
+    		with open("bot.token") as f:
+    			BOT_TOKEN = f.readline()
+    	return BOT_TOKEN
 
+    @staticmethod
+    def get_url():
+    	URL = os.environ.get('HEROKU_URL')
+    	return URL
 
 def autosending_text(bot, message):
     first_name = bot.get_chat(message.chat.id).first_name
-    text = """Hello, {0}""".format(first_name)
+    text = "Hello, {0}".format(first_name)
     return text
-
