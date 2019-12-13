e48cc87a827db2cb419721952f135a244c026cb5
snazau <snazarikov@gmail.com>
2019-12-13 19:24:17 +0200

    trash#1

--- bot.py
+++ bot.py
@@ -2,22 +2,22 @@ import telebot
 import logging
 from config import *
 
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')  # Ну это по классике, логи
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger()
 
-bot = telebot.TeleBot(Config.BOT_TOKEN)  # Создает объект класса "TeleBot", то есть нашего бота
-
+bot = telebot.TeleBot(Config.BOT_TOKEN)
 
 @bot.message_handler(
     content_types=['text', 'audio', 'document', 'photo', 'sticker', 'video', 'video_note', 'voice', 'location',
                    'contact', 'new_chat_members', 'left_chat_member', 'new_chat_title', 'new_chat_photo',
                    'delete_chat_photo', 'group_chat_created', 'supergroup_chat_created', 'channel_chat_created',
                    'migrate_to_chat_id', 'migrate_from_chat_id',
-                   'pinned_message'])  # декоратор который заставляет пользователя реагировать на новые сообщения
+                   'pinned_message'])
 def sending_auto2(message):
-    bot.send_message(chat_id=message.chat.id, text=autosending_text(bot, message), parse_mode='html',
-                     disable_web_page_preview=True)  # Отправляет авто сообщение
+    bot.send_message(chat_id=message.chat.id, text=autosending_text(bot, message), disable_web_page_preview=True) 
 
-
+if Config.MODE == PROD:
+	logger.debug("Config.MODE =", Config.MODE)
 if __name__ == '__main__':
+	logging.debug("Config.MODE", Config.MODE)
     bot.polling()  # Заставляет бота получать уведомления о новых сообщениях
--- config.py
+++ config.py
@@ -5,12 +5,11 @@ from telebot.types import InlineKeyboardButton
 
 class Config:
     BOT_TOKEN = os.environ['BOT_TOKEN']
+    MODE = os.environ['MODE']
 
 
 def autosending_text(bot, message):
     first_name = bot.get_chat(message.chat.id).first_name
-    text = """Hello, {0}
-This is the starter template for other bots built with <b>python</b>. 
-No bots cooked so far.
-Enjoy cooking with <a href = 'https://github.com/VadimCpp/pyfirstbotbot'>this template</a>!""".format(first_name)
+    text = """Hello, {0}""".format(first_name)
     return text
+
