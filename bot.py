1f9fdb34045c11c8573128c755613c55fded4268
snazau <snazarikov@gmail.com>
2019-12-24 13:37:24 +0200

    like pyfirstbot

--- bot.py
+++ bot.py
@@ -13,34 +13,14 @@ URL = Config.URL
 bot = telebot.TeleBot(TOKEN)
 server = Flask(__name__)
 
-# @bot.message_handler(
-#     content_types=['text', 'audio', 'document', 'photo', 'sticker', 'video', 'video_note', 'voice', 'location',
-#                    'contact', 'new_chat_members', 'left_chat_member', 'new_chat_title', 'new_chat_photo',
-#                    'delete_chat_photo', 'group_chat_created', 'supergroup_chat_created', 'channel_chat_created',
-#                    'migrate_to_chat_id', 'migrate_from_chat_id',
-#                    'pinned_message'])
-# def answer_smth(message):
-#     bot.send_message(chat_id=message.chat.id, text=autosending_text(bot, message), disable_web_page_preview=True)
-
-def sendMessage(message, text):
-	bot.send_message(message.chat.id, text)
-
-@bot.message_handler(commands=['start'])
-def send_info(message):
-	text = (
-	"<b>Welcome to the super bot 🤖!</b>\n"
-	"Say Hello to the bot to get a reply from it!"
-	)
-	bot.send_message(message.chat.id, text, parse_mode='HTML')
-
-@bot.message_handler(func=lambda msg: msg.text is not None)
-def reply_to_message(message):
-	if 'hello'in message.text.lower():
-		sendMessage(message, 'Hello! How are you doing today?')
-	else:
-		phrase = ["Spam bot in da house", "Please", "type", "smth", "with", "word", "hello"]
-		for word in phrase:
-			sendMessage(message, word)
+@bot.message_handler(
+    content_types=['text', 'audio', 'document', 'photo', 'sticker', 'video', 'video_note', 'voice', 'location',
+                   'contact', 'new_chat_members', 'left_chat_member', 'new_chat_title', 'new_chat_photo',
+                   'delete_chat_photo', 'group_chat_created', 'supergroup_chat_created', 'channel_chat_created',
+                   'migrate_to_chat_id', 'migrate_from_chat_id',
+                   'pinned_message'])
+def answer_smth(message):
+    bot.send_message(chat_id=message.chat.id, text=autosending_text(bot, message), disable_web_page_preview=True)
 
 @server.route('/' + TOKEN, methods=['POST'])
 def getMessage():
